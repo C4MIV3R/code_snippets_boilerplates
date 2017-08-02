7154c4ba35dfb9ec87c753ac6754cdd3165ca1a2
cam iverson <camiver@gmail.com>
2017-08-02 15:32:47 -0500

    optimized urls with $site_url variable

--- h_and_t_custom_code/404.php
+++ h_and_t_custom_code/404.php
@@ -4,9 +4,9 @@
 		<div class="row">
 			<div class="span_12">
 				<div id="psi_404 error-404">
-					<img src="http://hollyandtanager.com/wp-content/uploads/2017/05/giphy-downsized.gif" alt="404 - Kitty not found" class="psi_404_image">
+					<img src="'.$site_url.'/wp-content/uploads/2017/05/giphy-downsized.gif" alt="404 - Kitty not found" class="psi_404_image">
 					<h5 class="psi_404_header1" style="text-align:center; letter-spacing: 2px;">Kitty is just like you. Lost and confused.</h5>
-					<h6 class="psi_404_header2" style="text-align:center;">But never fear! Click here to go back <a href="http://hollyandtanager.com" class="button">home</a></h6>
+					<h6 class="psi_404_header2" style="text-align:center;">But never fear! Click here to go back <a href="'.$site_url.'" class="button">home</a></h6>
 				</div><!--/span_12 -->
 			</div>
 		</div><!--/row-->
