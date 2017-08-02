3038f8b6d10c62be42ac41a683c62c7361b86ad5
cam iverson <camiver@gmail.com>
2017-08-02 15:19:13 -0500

    renamed a few things for clarity

---
+++ h_and_t_custom_code/404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="container-wrap">
+	<div class="container main-content">
+		<div class="row">
+			<div class="span_12">
+				<div id="psi_404 error-404">
+					<img src="http://hollyandtanager.com/wp-content/uploads/2017/05/giphy-downsized.gif" alt="404 - Kitty not found" class="psi_404_image">
+					<h5 class="psi_404_header1" style="text-align:center; letter-spacing: 2px;">Kitty is just like you. Lost and confused.</h5>
+					<h6 class="psi_404_header2" style="text-align:center;">But never fear! Click here to go back <a href="http://hollyandtanager.com" class="button">home</a></h6>
+				</div><!--/span_12 -->
+			</div>
+		</div><!--/row-->
+	</div><!--/container-->
+</div>
+<?php get_footer(); ?>
