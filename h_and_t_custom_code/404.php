e52ac2bd9ceb92f51d7a3e05638e2151ebbfd20c
cam iverson <camiver@gmail.com>
2017-08-03 16:05:33 -0500

    such php, much wow, so amaze

--- h_and_t_custom_code/404.php
+++ h_and_t_custom_code/404.php
@@ -4,9 +4,9 @@
 		<div class="row">
 			<div class="span_12">
 				<div id="psi_404 error-404">
-					<img src="'.$site_url.'/wp-content/uploads/2017/05/giphy-downsized.gif" alt="404 - Kitty not found" class="psi_404_image">
+					<img src="<?php '.$site_url.' ?>/wp-content/uploads/2017/08/404_gif_not_found.gif" alt="404 - Kitty not found" class="psi_404_image">
 					<h5 class="psi_404_header1" style="text-align:center; letter-spacing: 2px;">Kitty is just like you. Lost and confused.</h5>
-					<h6 class="psi_404_header2" style="text-align:center;">But never fear! Click here to go back <a href="'.$site_url.'" class="button">home</a></h6>
+					<h6 class="psi_404_header2" style="text-align:center;">But never fear! Click here to go back <a href=" <?php '.$site_url.' ?>" class="button">home</a></h6>
 				</div><!--/span_12 -->
 			</div>
 		</div><!--/row-->
